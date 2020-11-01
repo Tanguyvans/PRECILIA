e053636fbae820891dfa9857779249a25a881959
vivian2114 <71881813+vivian2114@users.noreply.github.com>
2020-11-01 15:08:49 +0100

    Update createProjetDeRecherche.php
    Dans BD, attribut "Motcle" avec accent sur le e, peut etre ca qui foire

--- createProjetDeRecherche.php
+++ createProjetDeRecherche.php
@@ -8,21 +8,19 @@
 		$bdd = new PDO('mysql:host=localhost;dbname=PRECILIA', 'root', 'root');
 		echo "connection reussie avec la base de donnée<br>";
     //$bdd = new PDO($dsn, $username, $password);
-
+                  $IDProjet=$_POST['IDProjet']
 		  $Titre  = $_POST['Titre'];
-		 	$DateDebut = $_POST['DateDebut'];
+		  $DateDebut = $_POST['DateDebut'];
 		  $DateFin = $_POST['DateFin'];
-			$Description = $_POST['Description'];
-      $CollaborationAcademique = $_POST['CollaborationAcademique'];
+		  $Description = $_POST['Description'];
+                  $CollaborationAcademique = $_POST['CollaborationAcademique'];
 		  $CollaborationIndustrielle  = $_POST['CollaborationIndustrielle'];
-		 	$NumeroContact = $_POST['NumeroContact'];
+		  $NumeroContact = $_POST['NumeroContact'];
 		  $MotCle1 = $_POST['Motcle1'];
-			$Motcle2 = $_POST['MotCle2'];
+		  $Motcle2 = $_POST['MotCle2'];
 
-			$sql = "INSERT INTO PROJET DE RECHERCHE (IDProjet, Titre, DateDebut, DateFin, Description,
-        CollaborationAcademique, CollaborationIndustrielle, NumeroContact, MotCle1, MotCle2)
-			VALUES (NULL, '$Titre', '$DateDebut', '$DateFin', '$Description',
-        '$CollaborationAcademique','$CollaborationIndustrielle','$NumeroContact','$MotCle1', '$MotCle2')";
+			$sql = "INSERT INTO PROJET DE RECHERCHE (IDProjet, Titre, DateDebut, DateFin, Description, CollaborationAcademique, CollaborationIndustrielle, NumeroContact, MotCle1, MotCle2)
+			VALUES (NULL, '$Titre', '$DateDebut', '$DateFin', '$Description','$CollaborationAcademique','$CollaborationIndustrielle','$NumeroContact','$MotCle1', '$MotCle2')";
 
 			$Resultat = $bdd -> exec($sql);
 			echo "Ajout reussie avec la base de donnée<br>";
@@ -48,19 +46,19 @@
 		<label for="DateDebut">Date de début</label>
 		<input type="date" name="DateDebut" id="DateDebut">
 
-    <label for="DateFin">Date de fin</label>
+                <label for="DateFin">Date de fin</label>
 		<input type="date" name="DateFin" id="DateFin">
 
 		<label for="Description">Description</label>
 		<input type="text" name="Description" id="Description">
 
-		<label for="CollaborationAcademique">Collaboration academique</label>
+		<label for="CollaborationAcademique">Collaboration académique</label>
 		<input type="text" name="CollaborationAcademique" id="CollaborationAcademique">
 
 		<label for="CollaborationIndustrielle">Collaboration industrielle</label>
 		<input type="text" name="CollaborationIndustrielle" id="CollaborationIndustrielle">
 
-		<label for="NumeroContact">Numéro de la peronne de contact</label>
+		<label for="NumeroContact">Numéro de la personne de contact</label>
 		<input type="number" name="NumeroContact" id="NumeroContact">
 
 		<label for="MotCle1">Le mot clé principal de la publication</label>
@@ -74,4 +72,4 @@
 
     <a href="index.php">Retour en arrière</a>
 
-	<? php include "templates/footer.php" ?>
+	<?php include "templates/footer.php" ?>
