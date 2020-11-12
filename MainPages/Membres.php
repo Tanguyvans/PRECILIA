976da0cc68dfe6d7dca615d6f7722dfdd0c96f19
Vivian Ruelle <vivian.ruelle2108@gmail.com>
2020-11-12 19:15:05 +0100

    affichage accueil topissime+debut page membre

--- MainPages/Membres.php
+++ MainPages/Membres.php
@@ -1,2 +1,33 @@
 <?php include '../templates/header.php' ?>
-<?php include '../templates/footer.php' ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <!--adaptive response to the width of the device-->
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>PRESCILIA - Membres</title>
+        <!--Description du site lors de la recherche-->
+        <meta content="" name="descriptison">
+        <meta content="" name="keywords">
+        <link rel="stylesheet" href="../css/pageMembre.css"/>
+
+    </head>
+  <body>
+    <?php include '../templates/header.php' ?>
+    <div class="wrapper">
+        <div>Un</div>
+        <div>Deux</div>
+        <div>Trois</div>
+        <div>Quatre</div>
+        <div>Cinq</div>
+    </div>
+
+
+
+    <?php include '../templates/footer.php' ?>
+  </body>
+
+</html>
+
+</html>
