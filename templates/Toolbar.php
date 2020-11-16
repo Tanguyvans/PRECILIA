b3d4b6e9b0e079b277ab32ab86f1e573ecbd9a50
Vivian Ruelle <vivian.ruelle2108@gmail.com>
2020-11-16 10:24:33 +0100

    curseur tableau+toolbar

---
+++ templates/Toolbar.php
@@ -0,0 +1,28 @@
+<html lang="fr">
+
+<head>
+    <!---->
+
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Header</title>
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+    <!-- Template Main CSS File -->
+    <link href="../css/pageT.css" rel="stylesheet">
+</head>
+<body>
+<header id="header">
+    <div id="container">
+        <div id="container1">
+            <a href='../MainPages/Accueil.php'> <h1> Prescilia </h1> </a>
+        </div>
+        <nav id="container2">
+            <div class="active"><a href='../MainPages/adresseprofil'>Profil</a></div>
+            <div class="active"><a href='../MainPages/Ajout.php'>Ajouter</a></div>
+            <div class="active"><a href='../MainPages/adresse'>Autre?</a></div>
+        </nav>
+    </div>
+</header>
+</body>
+</html>
