ac9669632a6c8fe32742c9c505811bf3513a13c4
tanguy <tanguyvans@gmail.com>
2020-11-20 16:24:06 +0100

    Afficher motcle amelioré

--- Formulaires/affichageThese.php
+++ Formulaires/affichageThese.php
@@ -3,33 +3,30 @@
     $bdd = new PDO($dsn, $username, $password);
 ?>
 
-<?php
-    try{
-        $sql = "SELECT * FROM These";
-        $resultat = $bdd->query($sql);
-
-    }
-    catch (Exception $e){
-        die('Erreur : '.$e->getMessage());
-    }
-?>
-
 <form method="post">
     <!--========== Mot cle 1 ==============-->
     <?php
     $result = $bdd->query('SELECT DISTINCT MotCle1 FROM These');
     foreach ($result as $row) {
-        $MC1[] = array('MotCle1' => $row['MotCle1']);
+        $MCA[] = array('MotCle1' => $row['MotCle1']);
     }
+    $result = $bdd->query('SELECT DISTINCT MotCle2 FROM These');
+    foreach ($result as $row) {
+        $MCB[] = array('MotCle1' => $row['MotCle2']);
+    }
+
+    $MC1 = array_merge($MCA, $MCB);
+    $MC1 = array_unique($MC1, SORT_REGULAR);
     ?>
 
     <label for="MotCle1">MotCle1</label>
     <select name="MotCle1" id="MotCle1">
-        <option value="">Select one</option>
+        <option value="All">Select one</option>
         <?php foreach ($MC1 as $test): ?>
             <option value="<?php print_r($test['MotCle1']); ?>"><?php print_r($test['MotCle1']);?></option>
         <?php endforeach; ?>
     </select>
+
     <!--========== Mot cle 2 ==============-->
     <?php
     $result = $bdd->query('SELECT DISTINCT MotCle2 FROM These');
@@ -40,17 +37,44 @@
 
     <label for="MotCle2">MotCle2</label>
     <select name="MotCle2" id="MotCle2">
-        <option value="">Select one</option>
+        <option value="All">Select one</option>
         <?php foreach ($MC2 as $test): ?>
             <option value="<?php print_r($test['MotCle2']); ?>"><?php print_r($test['MotCle2']);?></option>
         <?php endforeach; ?>
     </select>
 
-    <input type="submit" name="submit" value="Submit">
+    <input type="submit" name="Recherche" value="Recherche">
 
 </form>
+<!--========== Il faut appuyer sur le boutton recherche ==============-->
+<?php
+if (isset($_POST['Recherche'])) {
+    try{
+        $MotCle1  = $_POST['MotCle1'];
+        $MotCle2 = $_POST['MotCle2'];
+
+        if($MotCle1 == "All" and $MotCle2 == "All"){
+            $sql = "SELECT * FROM These";
+            $resultat = $bdd->query($sql);
+        }elseif ($MotCle1 != "All" and $MotCle2 == "All"){
+            $sql = "SELECT * FROM These WHERE MotCle1= '$MotCle1'";
+            $resultat = $bdd->query($sql);
+        }elseif ($MotCle1 == 'All' and $MotCle2 != "All"){
+            $sql = "SELECT * FROM These WHERE MotCle2= '$MotCle2'";
+            $resultat = $bdd->query($sql);
+        }else{
+            $sql = "SELECT * FROM These 
+                    WHERE MotCle1= '$MotCle1' 
+                    AND MotCle2= '$MotCle2'";
+            $resultat = $bdd->query($sql);
+        }
 
 
+    }
+    catch (Exception $e){
+        die('Erreur : '.$e->getMessage());
+    }
+?>
 <!--========== Tableau ==============-->
 <section>
     <!-- contruction de la table-->
@@ -94,3 +118,6 @@
         ?>
     </table>
 </section>
+<?php
+}
+?>
